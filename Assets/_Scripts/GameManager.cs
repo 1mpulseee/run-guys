b3f04edb58ded22e79e520e15a24d9d634d6c020
1mpulseee <lopd.2004@gmail.com>
2022-11-05 14:44:06 +0300

    папочки

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Player;
+    public Transform SpawnPos;
+    public void Awake()
+    {
+        GameObject NewPlayer = PhotonNetwork.Instantiate(Player.name, SpawnPos.position, SpawnPos.rotation);
+    }
+}
